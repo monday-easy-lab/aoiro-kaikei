e451194acfc57a58cf8ebcaf6150a2c79977ff12
ak201107160225 <a.k.201107160225@gmail.com>
2026-06-04 18:59:57 +0900

    fix: 誤って追加したaoiro-kaikeiフォルダを削除

--- aoiro-kaikei/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// GitHub Pages にデプロイする場合は base をリポジトリ名に合わせる
-// 例: https://username.github.io/aoiro-kaikei/
-export default defineConfig({
-  plugins: [react()],
-  base: "/aoiro-kaikei/",
-});
